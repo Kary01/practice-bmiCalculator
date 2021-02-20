ad79e4b7b590276658ffe25c4e1f6c6931e662f0
Karen Ruiz <anakaren.ruizb@gmail.com>
2021-02-19 21:54:29 -0600

    change the font color

--- js/app.js
+++ js/app.js
@@ -1,37 +1,44 @@
 
-
 function calculate(){
   
   let bmi;
   let result;
   let category;
 
-  // 1. Get value from the sliders and display them with "oninput element" on html
+  // Get value from the sliders and display them with "oninput element" on html
 
   let weight = parseInt(document.getElementById('weight').value);
   document.getElementById('weight-value').textContent = weight + ' kg';
+
   let height = parseInt(document.getElementById('height').value);
   document.getElementById('height-value').textContent = height + ' cm';
 
-  // 2. Calculate to BMI
+  // Calculate to BMI
   bmi = (weight / Math.pow((height/100), 2)).toFixed(1);
   result = document.getElementById('result');
   result.textContent = bmi;
 
-  // 3. Assign a category based on the bmi
+  // Assign a category based on the bmi
 
   if (bmi < 18.5) {
     category = "Underweight";
+
+    // Change the font color based on category
+
+    result.style.color = "#ffc44d";
+
   }else if(bmi >= 18.5 && bmi <= 24.9){
     category = "Normal weight";
+    result.style.color = "#0be881";
+
   }else if(bmi >= 25 && bmi <= 29.9){
     category = "Overweight";
+    result.style.color = "#ff884d";
+
   }else {
     category = "Obese";
+    result.style.color = "#ff5e57";
   }
 
   document.getElementById('category').textContent = category;
-
 }
-
-calculate();
